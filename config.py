c44041ab055ab71a53d277db1864e74a6a383170
Prabhjot Singh <singhjot0020@gmail.com>
2025-06-18 02:01:06 +0530

    changed to local mongo db

--- config.py
+++ config.py
@@ -2,8 +2,10 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 uri = "mongodb+srv://pr4bh7ot:El5gMdUcaKganHjX@mongo-fastapi.mjhxjpt.mongodb.net/?retryWrites=true&w=majority&appName=Mongo-FastAPI"
+# client = MongoClient(uri, server_api=ServerApi("1"))
 
-client = MongoClient(uri, server_api=ServerApi("1"))
+# Switched to local MongoDB for development
+client = MongoClient("mongodb://localhost:27017/")
 
 db = client.todo_db
 collection = db["todo_data"]
